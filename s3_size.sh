7c8a2602decaa353f7c2ff0306c87af192a9ae64
Aron Neagu <aron.neagu@sainsburys.co.uk>
2019-01-17 15:19:37 +0000

    format output as table

--- s3_size.sh
+++ s3_size.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# TODO
+# - add argument to output to table format
+# - use default aws profile unless a different one is specified at command line
 size=0
 current_size=0
 objects=0
@@ -7,23 +10,21 @@ index=1
 aws_profile=$1
 total_number_of_buckets=$(aws --profile $aws_profile s3 ls|wc -l|sed 's/\ //g')
 list_of_buckets=$(aws --profile $aws_profile s3 ls|cut -f3 -d" ")
+echo "Bucket	Size		Number of objects"
 for bucket in $list_of_buckets
   do 
      current_bucket=$(aws --profile $aws_profile s3 ls --recursive --summarize $bucket|grep -e 'Total Size' -e 'Total Objects')
-     echo "[${index}/${total_number_of_buckets}] $bucket"
      # Calculate the size of bucket
      current_size=$(echo "$current_bucket"|grep "Total Size"|tr -s [:space:]|cut -f2 -d":"|sed -e 's/\ //g')
      size=$((size+current_size))
-     echo "Current size: $current_size"
-     echo "Running size: $size"
      # Calculate number of objects per bucket
      current_objects=$(echo "$current_bucket"|grep "Total Objects"|tr -s [:space:]|cut -f2 -d":"|sed -e 's/\ //g')
      objects=$((objects+current_objects))
-     echo "Current objects: $current_objects"
-     echo "Running objects: $objects"
+     echo -en "[${index}/${total_number_of_buckets}] $bucket \t"
+     echo -en "$current_size \t"
+     echo "$current_objects"
      ((index++))
 done
 
-echo -e "\n\n\n"
 echo "Total size: $size bytes"
 echo "Total objects: $objects"
