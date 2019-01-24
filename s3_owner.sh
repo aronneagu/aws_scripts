49d7749682785f91fc5a877e4d5ea1d1625ddb69
Aron Neagu <aron.neagu@sainsburys.co.uk>
2019-01-24 14:50:47 +0000

    update s3_owner check that aws_profile is passed

--- s3_owner.sh
+++ s3_owner.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Will print buckets that don't have "owner" tag
+if [ "$#" -ne 1 ]
+  then
+    echo "Usage: $(basename $0) <aws_profile>"
+    exit 1
+fi
 aws_profile=$1
 for bucket in $(aws --profile $aws_profile s3 ls|tr -s [:space:]|cut -f3 -d" ")
 do
   bucket_owner=''
-  bucket_owner=$(aws --output text --profile $aws_profile s3api get-bucket-tagging --bucket $bucket --query "TagSet[?Key=='owner'].Value|[0]" 2>/dev/null)
-  if [ -z "$bucket_owner" ]
-    then
-      echo $bucket
-  fi
+  bucket_owner=$(aws --output text --profile $aws_profile s3api get-bucket-tagging --bucket $bucket --query "TagSet[?Key=='date_owner'].Value|[0]" 2>/dev/null)
+  echo $bucket $bucket_owner
 done
