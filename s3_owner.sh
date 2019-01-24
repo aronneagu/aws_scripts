8b1650a72fca802e19e2cd455da80c6eef4005ec
Aron Neagu <aron.neagu@sainsburys.co.uk>
2019-01-24 15:08:01 +0000

    read AWS_PROFILE or AWS_DEFAULT_PROFILE in case they are defined

--- s3_owner.sh
+++ s3_owner.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # Will print buckets that don't have "owner" tag
-if [ "$#" -ne 1 ]
-  then
+if [ "$#" -eq 1 ]; then
+    aws_profile=$1
+elif [ -n "$AWS_DEFAULT_PROFILE" ]; then
+    aws_profile="$AWS_DEFAULT_PROFILE"
+elif [ -n "$AWS_PROFILE" ]; then
+    aws_profile="$AWS_PROFILE"
+else
     echo "Usage: $(basename $0) <aws_profile>"
     exit 1
 fi
-aws_profile=$1
+
 for bucket in $(aws --profile $aws_profile s3 ls|tr -s [:space:]|cut -f3 -d" ")
 do
   bucket_owner=''
