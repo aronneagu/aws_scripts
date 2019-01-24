84783b3348035e34f58af41985511c2429971dd0
Aron Neagu <aron.neagu@sainsburys.co.uk>
2019-01-24 16:05:51 +0000

    update s3_size add check for aws_profile

--- s3_size.sh
+++ s3_size.sh
@@ -2,12 +2,24 @@
 # TODO
 # - add argument to output to table format
 # - use default aws profile unless a different one is specified at command line
+# - add -h argument to include header
+
+if [ "$#" -eq 1 ]; then
+    aws_profile=$1
+elif [ -n "$AWS_DEFAULT_PROFILE" ]; then
+    aws_profile="$AWS_DEFAULT_PROFILE"
+elif [ -n "$AWS_PROFILE" ]; then
+    aws_profile="$AWS_PROFILE"
+else
+    echo "Usage: $(basename "$0") <aws_profile>"
+    exit 1
+fi
+
 size=0
 current_size=0
 objects=0
 current_objects=0
 index=1
-aws_profile=$1
 total_number_of_buckets=$(aws --profile $aws_profile s3 ls|wc -l|sed 's/\ //g')
 list_of_buckets=$(aws --profile $aws_profile s3 ls|cut -f3 -d" ")
 echo "Bucket	Size		Number of objects"
