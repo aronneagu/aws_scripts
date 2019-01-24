ac85494eeab724dac210b7a089e6aa368169b4bb
Aron Neagu <aron.neagu@sainsburys.co.uk>
2019-01-24 15:35:35 +0000

    fix shellcheck errors

--- s3_owner.sh
+++ s3_owner.sh
@@ -7,13 +7,17 @@ elif [ -n "$AWS_DEFAULT_PROFILE" ]; then
 elif [ -n "$AWS_PROFILE" ]; then
     aws_profile="$AWS_PROFILE"
 else
-    echo "Usage: $(basename $0) <aws_profile>"
+    echo "Usage: $(basename "$0") <aws_profile>"
     exit 1
 fi
 
-for bucket in $(aws --profile $aws_profile s3 ls|tr -s [:space:]|cut -f3 -d" ")
+tmpfile=$(mktemp "${TMPDIR:-/tmp/}$(basename "$0").XXXX")
+
+for bucket in $(aws --profile "$aws_profile" s3 ls|tr -s '[:space:]'|cut -f3 -d" ")
 do
   bucket_owner=''
-  bucket_owner=$(aws --output text --profile $aws_profile s3api get-bucket-tagging --bucket $bucket --query "TagSet[?Key=='date_owner'].Value|[0]" 2>/dev/null)
-  echo $bucket $bucket_owner
+  bucket_owner=$(aws --output text --profile "$aws_profile" s3api get-bucket-tagging --bucket "$bucket" --query "TagSet[?Key=='date_owner'].Value|[0]" 2>/dev/null)
+  echo "$bucket $bucket_owner" >> "$tmpfile"
 done
+column -t "$tmpfile"
+rm -f $"tmpfile"
